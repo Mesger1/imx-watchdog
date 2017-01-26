26da25e13839c1d40262cd3e62beae17a21677ff
Gerd Mestdagh <Gerd Mestdagh>
2017-01-26 19:58:13 +0000

    sudo

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-mount -o remount,rw /
-update-rc.d -f imx-watchdog remove
-update-rc.d -f imx-keepalive remove
-update-rc.d -f watchdog remove
-update-rc.d -f wd_keepalive remove
+sudo mount -o remount,rw /
+sudo update-rc.d -f imx-watchdog remove
+sudo update-rc.d -f imx-keepalive remove
+sudo update-rc.d -f watchdog remove
+sudo update-rc.d -f wd_keepalive remove
 sudo apt-get update
 sudo apt-get install git
 git clone https://github.com/gerdmestdagh/imx-watchdog
-cd imx-watchdog
-cp ./devregs /usr/bin
-cp ./imx-pinger /usr/bin
-cp ./imx-watchdog /etc/init.d
-cp ./imx-keepalive /etc/init.d
-update-rc.d -f imx-watchdog start 1 S .
-update-rc.d -f imx-keepalive start 1 2 .
+sudo cd imx-watchdog
+sudo cp ./devregs /usr/bin
+sudo cp ./imx-pinger /usr/bin
+sudo cp ./imx-watchdog /etc/init.d
+sudo cp ./imx-keepalive /etc/init.d
+sudo update-rc.d -f imx-watchdog start 1 S .
+sudo update-rc.d -f imx-keepalive start 1 2 .
